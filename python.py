1da1f8d69ac422202d79fc4e6254d8f395be67d0
me <me>
2026-03-23 10:12:18 +0530

    modified

--- python.py
+++ python.py
@@ -1,8 +1,4 @@
-import random
-secret=random.randint(1,10)
-guess=int(input("Guess a number between 1 and 10"))
-if guess==secret:
-    print("Correct!")
-else:
-    print("Wrong!",secret)
-
+a=10
+b=50
+c=a+b
+print(c)
